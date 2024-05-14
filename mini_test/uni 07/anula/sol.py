3915ffa9dc2409824b25e9f5fc85f0436cf427d9
Ruan Rodrigues <minip0607@gmail.com>
2024-05-14 13:22:30 -0300

    test

--- mini_test/uni 07/anula/sol.py
+++ mini_test/uni 07/anula/sol.py
@@ -1,16 +1,18 @@
 def anula(lista):
-    if len(lista) % 2 == 0:
-        for i in range(len(lista) -2, 0, -1):
-            if lista[i] + lista[i - 1] == 0:
-                lista.pop(i)
-                lista.pop(i - 1)
-    else:
-        for i in range(len(lista) -1, 1, -1):
-            if lista[i] + lista[i - 1] == 0:
-                lista.pop(i)
-                lista.pop(i - 1)
-    
-l = [0, 0]
+    for i in range(len(lista) -1 , 0, -2):
+        if lista[i] + lista[i - 1] == 0:
+            lista.pop(i)
+            lista.pop(i - 1)
+
+
+l = [-1, 1, -1, 1, -1]
 anula(l)
-print(l)
-print(l)
+assert l == [-1]
+
+l2 = [0, 0, 0]
+anula(l2)
+assert l2 == [0]
+
+l3 = [0, 0, 0, 0]
+anula(l3)
+assert l3 == []
